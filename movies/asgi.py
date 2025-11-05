289fd2c1dc665d682a13143aeaf6ef86da31ed82
Sergey Tsepilov <srgdevkz@mail.ru>
2025-11-05 12:39:54 +0500

    asgi

---
+++ movies/asgi.py
@@ -0,0 +1,9 @@
+"""ASGI-конфигурация проекта Movies."""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "movies.settings")
+
+application = get_asgi_application()
